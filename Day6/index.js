43260d8a9e9e76680abc92f3b50e4800d6da2e56
vandanabhay1997@gmail.com <vandanabhay1997@gmail.com>
2025-01-13 10:55:10 +0530

    my day 6

---
+++ Day6/index.js
@@ -0,0 +1,10 @@
+// let inputBtn = document.getElementById("input-btn")
+
+// inputBtn.addEventListener("click", function() {
+//     console.log("Button clicked from addEventListener")
+// })
+
+let inputBtn = document.getElementById("box")
+inputBtn.addEventListener("click", function() {
+    console.log("I want to open the box!");
+})
