04d353dd5690e639c292f6aae6b7a59eff3288e8
vandanabhay1997@gmail.com <vandanabhay1997@gmail.com>
2025-01-20 11:01:36 +0530

    My day 11 study

---
+++ Day12/index.js
@@ -0,0 +1,23 @@
+// const welcomeEl = document.getElementById("welcome-el")
+
+// function greetUser(greeting, name, emoji) {
+//     // Rewrite the expression using template literals
+//     welcomeEl.textContent = `${greeting} , ${name} + ${emoji}`
+// }
+// greetUser("Howdy", "James" , " 👋")
+
+// Create a function, add(), that adds two numbers together and returns the sum
+
+
+function add(num1, num2) {
+    return num1 + num2
+
+
+}
+
+console.log( add(3, 4)   ) // should log 7
+console.log( add(9, 102) ) // should log 111
+// What are greeting and name? //parameters
+// What are "Howdy" and "James"? //arguemnts
+// What are num1 and num2? // parameters
+// What are 3 and 4? // arguments
