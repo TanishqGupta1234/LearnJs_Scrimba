f42f6b382e0520e427bcc25c9ee781e99cb07686
vandanabhay1997@gmail.com <vandanabhay1997@gmail.com>
2025-01-22 10:04:20 +0530

    My final changes of day 13

---
+++ Day13/test.js
@@ -0,0 +1,11 @@
+// Create a function, getFirst(arr), that returns the first item in the array
+
+function getFirst(arr) {
+    return arr[0];
+}
+let first = getFirst([1,2,3,4])
+console.log(first);
+
+
+
+// Call it with an array as an argument to verify that it works
